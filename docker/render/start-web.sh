db27feaf3ec1139924764be0be73db50f0fc49cd
matejcv24 <matejcvetanovski@hotmail.com>
2026-04-28 11:49:33 +0200

    deploy fix

--- docker/render/start-web.sh
+++ docker/render/start-web.sh
@@ -5,6 +5,7 @@ cd /var/www/html
 
 mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views storage/logs bootstrap/cache
 
+php artisan migrate --force
 php artisan storage:link || true
 php artisan config:cache
 php artisan view:cache
